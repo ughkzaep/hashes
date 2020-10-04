aeefb552639c90127db1efd77b988b94ad4f1ec2
ughkzaep <nrn312+ughkzaep@gmail.com>
2020-10-04 14:54:25 -0400

    added drive and drivefile tables

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,12 @@ create table filekeys(id serial not null primary key,
                       filevalue char(64) not null unique,
                       filesize bigint not null);
 
+create table drive (id serial not null primary key, 
+                    vsn char(9) not null unique, 
+                    drivename varchar(32) not null unique);
+
+create table drivefile(id serial not null primary key, 
+                       did bigint not null references drive(id),
+                       fid bigint not null references filekeys(id), 
+                       unique(did, fid));
+
